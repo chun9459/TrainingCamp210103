ed092de23faca87a53593f1607b0084495c5e943
Yi-Chun Chung <76869260+chun9459@users.noreply.github.com>
2021-03-21 22:10:24 +0800

    Create HomeWork4.py

---
+++ 1-4/HomeWork4.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import render_template
+from flask import session
+
+app = Flask(__name__) 
+app.secret_key = "haohgwpejflanwiP3IRPF"
+
+@app.route("/")
+def index():       
+    return render_template("HomeWork4.html")
+
+
+@app.route("/siqnin", methods=["POST"])
+def siqnin():
+    getid = request.form["yourid"]
+    getpassword = request.form["yourpassword"]
+    session["getid"] = getid
+    session["getpassword"] = getpassword
+    if getid == "test" and getpassword == "test" :
+        return redirect("/member/")
+    else :
+        return redirect("/error/")
+
+@app.route("/member/")
+def member(): 
+    if "getid" in session:
+        getid = session["getid"]
+        return render_template("member.html")
+    else:
+        return redirect("/")
+
+@app.route("/error/")
+def error():     
+    return render_template("error.html")
+
+@app.route("/signout")
+def logout():
+   session.pop("getid", None)
+   return redirect("/")
+
+app.run(port=3000)
