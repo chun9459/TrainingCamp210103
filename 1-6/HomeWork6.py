0af2cb7f639e56f3f26f2a5324f1a03ed1e4359f
Yi-Chun Chung <76869260+chun9459@users.noreply.github.com>
2021-04-05 15:04:32 +0800

    Update HomeWork6.py

--- 1-6/HomeWork6.py
+++ 1-6/HomeWork6.py
@@ -5,6 +5,7 @@ from flask import render_template
 from flask import session
 from flask import url_for
 
+# 連線 MySQL
 import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
@@ -17,25 +18,30 @@ mycursor = mydb.cursor()
 app = Flask(__name__)
 app.secret_key = "haohgwpejflanwiP3IRP!F" 
 
+# 首頁
 @app.route("/")
 def index():       
     return render_template("HomeWork6.html")
 
+# 註冊
 @app.route("/signup", methods=["POST"])
 def signup():
     getname = request.form["name"]
     getusername = request.form["username"]
     getpassword = request.form["password"]
-    mycursor.execute("SELECT * FROM user WHERE username = '"+getusername+"'")
-    myresult = mycursor.fetchone()
-    if myresult == None:
-        mycursor.execute("INSERT INTO user(name, username, password) VALUES ('"+getname+"','"+getusername+"','"+getpassword+"')")
-        mydb.commit()
-        return redirect("/")
+    if not getname.strip() or not getusername.strip() or not getpassword.strip():
+        return redirect(url_for("error", message = "資料不得為空"))
     else:
-        return redirect(url_for("error", message = "帳號已經被註冊"))
+        mycursor.execute("SELECT * FROM user WHERE username = '"+getusername+"'")
+        myresult = mycursor.fetchone()
+        if myresult == None:
+            mycursor.execute("INSERT INTO user(name, username, password) VALUES ('"+getname+"','"+getusername+"','"+getpassword+"')")
+            mydb.commit()
+            return redirect("/")
+        else:
+            return redirect(url_for("error", message = "帳號已經被註冊"))       
 
-
+# 登入
 @app.route("/signin", methods=["POST"])
 def signin():
     getusername = request.form["username"]
@@ -49,6 +55,7 @@ def signin():
     else :
         return redirect(url_for("error", message = "帳號或密碼輸入錯誤"))
 
+# 會員頁
 @app.route("/member/")
 def member(): 
     if "getusername" in session:
@@ -59,11 +66,13 @@ def member():
     else:
         return redirect("/")
 
+# 錯誤頁
 @app.route("/error/")
 def error():
     message = request.args.get("message","")
     return render_template("error.html", message = message )
 
+# 登出
 @app.route("/signout")
 def signout():
    session.pop("getusername", None)
